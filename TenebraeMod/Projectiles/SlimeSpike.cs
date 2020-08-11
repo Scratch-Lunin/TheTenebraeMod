8dcfb76d420fe18448c02cd33041a83e09549bc9
Scratch-Lunin <67486775+Scratch-Lunin@users.noreply.github.com>
2020-08-11 00:00:49 -0700

    Add files via upload
    Added Slime Spike Projectile

---
+++ TenebraeMod/Projectiles/SlimeSpike.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TenebraeMod.Projectiles
+{
+	public class SlimeSpike : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Friendly Slime Spike");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 10;
+			projectile.height = 20;
+			projectile.aiStyle = 1;
+			projectile.friendly = true;
+			projectile.melee = true;
+			projectile.aiStyle = 1;
+			projectile.damage = 10;
+		}
+
+
+
+		public override void PostAI()
+		{
+			if (Main.rand.NextBool())
+			{
+				Dust dust = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, 253);
+				dust.noGravity = false;
+				dust.scale = 0.4f;
+			}
+		}
+	}
+}
