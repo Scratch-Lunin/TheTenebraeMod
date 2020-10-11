34bd4a1b70510ba99afb06a9da2df4494ec1a681
Scratch-Lunin <67486775+Scratch-Lunin@users.noreply.github.com>
2020-10-11 10:56:25 -0700

    Added Starlight Block

---
+++ TenebraeMod/Tiles/StarlightBlock.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TenebraeMod.Tiles
+{
+	public class StarlightBlock : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileMergeDirt[Type] = false;
+			Main.tileBlockLight[Type] = true;
+			Main.tileLighted[Type] = true;
+			dustType = 1;
+			drop = mod.ItemType("StarlightBlock");
+			AddMapEntry(new Color(240, 213, 34));
+			// Set other values here
+		}
+    }
+}
