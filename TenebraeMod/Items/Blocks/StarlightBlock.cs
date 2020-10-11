1610d6b46f3c11bac05db9946bac4aab3db39de1
Scratch-Lunin <67486775+Scratch-Lunin@users.noreply.github.com>
2020-10-11 10:57:11 -0700

    Added Starlight Block

---
+++ TenebraeMod/Items/Blocks/StarlightBlock.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using System;
+using static Terraria.ModLoader.ModContent;
+
+namespace TenebraeMod.Items.Blocks
+{
+	public class StarlightBlock : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.useTurn = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.autoReuse = true;
+			item.maxStack = 999;
+			item.consumable = true;
+			item.createTile = mod.TileType("StarlightBlock");
+			item.width = 16;
+			item.height = 16;
+			item.value = 100;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.FallenStar, 20);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
