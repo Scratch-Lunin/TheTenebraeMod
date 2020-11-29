cb74081d08220261c05dc778ed6b8277565489db
nicobrownmath <65060750+nicobrownmath@users.noreply.github.com>
2020-11-28 20:00:47 -0800

    Update HolyflameDust.cs
    Added pink holyflame dust

--- TenebraeMod/Dusts/HolyflameDust.cs
+++ TenebraeMod/Dusts/HolyflameDust.cs
@@ -30,4 +30,33 @@ namespace TenebraeMod.Dusts
         public override Color? GetAlpha(Dust dust, Color lightColor)
 			=> new Color(lightColor.R, lightColor.G, lightColor.B, 25);
 	}
+	
+	public class PinkHolyflameDust : ModDust
+	{
+		public override void OnSpawn(Dust dust)
+		{
+			dust.noGravity = true;
+			dust.scale = 2f;
+		}
+
+		public override bool Update(Dust dust)
+		{
+			dust.position += dust.velocity / 2;
+			dust.rotation += dust.velocity.X / 2;
+			dust.scale *= 0.95f;
+			if (dust.scale < 0.1f)
+			{
+				dust.active = false;
+			}
+			else
+			{
+				float strength = dust.scale * 1.4f;
+				Lighting.AddLight((int)(dust.position.X / 16f), (int)(dust.position.Y / 16f), strength * 0.7f, strength * 0.65f, strength * 0.3f);
+			}
+			return false;
+		}
+
+		public override Color? GetAlpha(Dust dust, Color lightColor)
+			=> new Color(lightColor.R, lightColor.G, lightColor.B, 25);
+	}
 }
