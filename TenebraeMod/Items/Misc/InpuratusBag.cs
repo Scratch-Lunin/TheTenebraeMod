9cc79042bee2199d72ff28b7a7e0c319d5710484
Scratch-Lunin <67486775+Scratch-Lunin@users.noreply.github.com>
2021-03-28 15:36:35 -0700

    Added Inpuratus Bag

---
+++ TenebraeMod/Items/Misc/InpuratusBag.cs
@@ -0,0 +1,52 @@
+﻿using TenebraeMod.Items.Weapons;
+using TenebraeMod.NPCs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+
+namespace TenebraeMod.Items.Misc
+{
+	public class InpuratusBag : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Treasure Bag");
+			Tooltip.SetDefault("{$CommonItemTooltip.RightClickToOpen}");
+		}
+
+		public override void SetDefaults()
+		{
+			item.maxStack = 999;
+			item.consumable = true;
+			item.width = 40;
+			item.height = 42;
+			item.rare = ItemRarityID.Pink;
+			item.expert = true;
+		}
+
+		public override bool CanRightClick()
+		{
+			return true;
+		}
+
+		public override void OpenBossBag(Player player)
+		{
+			player.TryGettingDevArmor();
+			player.TryGettingDevArmor();
+			player.QuickSpawnItem(ItemID.CursedFlame, 20 + Main.rand.Next(10));
+			player.QuickSpawnItem(ItemID.RottenChunk, 50 + Main.rand.Next(10));
+
+			var dropChooser = new WeightedRandom<int>();
+			dropChooser.Add(ItemID.ClingerStaff, 5);
+			dropChooser.Add(ModContent.ItemType<Items.Weapons.VileGlaive>(), 5);
+			dropChooser.Add (ModContent.ItemType<Items.Weapons.CursedCannon>(), 5);
+			player.QuickSpawnItem(dropChooser);
+			int choice = dropChooser;
+
+			player.QuickSpawnItem(ItemID.WormScarf);
+		}
+
+		public override int BossBagNPC => ModContent.NPCType<Inpuratus>();
+	}
+}
