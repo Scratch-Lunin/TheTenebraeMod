76a7e58fda0bc27944900f6048f0be8af6ec18ba
Scratch-Lunin <67486775+Scratch-Lunin@users.noreply.github.com>
2021-06-25 01:09:23 -0700

    Added a bunch of Unsafe Dungeon Walls

---
+++ TenebraeMod/Items/Tiles/Walls/GreenDungeonTileUnsafe.cs
@@ -0,0 +1,58 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TenebraeMod.Items.Tiles.Walls
+{
+    public class GreenDungeonTileUnsafe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Unsafe Green Tiled Wall");
+            Tooltip.SetDefault("Places unsafe Dungeon walls" + "\nEnemies will spawn when enough walls and Dungeon brick are placed" + "\nCan only be mined at its edge");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 28;
+            item.maxStack = 999;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useTime = 15;
+            item.useAnimation = 15;
+            item.value = 0;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.rare = ItemRarityID.White;
+            item.createWall = 99;
+            // Set other item.X values here
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod); // 1 Brick ---> 4 Unsafe Walls
+            recipe.AddIngredient(ItemID.GreenBrick);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this, 4);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(mod); // 1 Wall ---> 1 Unsafe Wall
+            recipe.AddIngredient(ItemID.GreenTiledWall);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(mod); // 1 Unsafe Wall ---> 1 Wall
+            recipe.AddIngredient(null, "GreenDungeonTileUnsafe");
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(ItemID.GreenTiledWall);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(mod); // 1 Brick ---> 4 Walls
+            recipe.AddIngredient(ItemID.GreenBrick);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(ItemID.GreenTiledWall, 4);
+            recipe.AddRecipe();
+        }
+    }
+}
