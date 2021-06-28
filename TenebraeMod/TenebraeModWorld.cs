edcd4045e172d9d8a0592ab8b401208963caf597
Scratch-Lunin <67486775+Scratch-Lunin@users.noreply.github.com>
2021-06-28 14:58:36 -0700

    how tf is tenebraemodworld completely absent

---
+++ TenebraeMod/TenebraeModWorld.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ModLoader;
+
+namespace TenebraeMod
+{
+    public class TenebraeModWorld : ModWorld
+    {
+        public static bool InpuratusDies = false;
+        public static bool DashShake = false;
+    }
+}
