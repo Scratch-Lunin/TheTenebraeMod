330c75ac038c35d6df46bdf0963cdc4c5d3bc8c5
Scratch-Lunin <67486775+Scratch-Lunin@users.noreply.github.com>
2021-07-05 03:00:49 -0700

    Add files via upload

--- TenebraeMod/TenebraeModWorld.cs
+++ TenebraeMod/TenebraeModWorld.cs
@@ -5,6 +5,7 @@ namespace TenebraeMod
     public class TenebraeModWorld : ModWorld
     {
         public static bool InpuratusDies = false;
+        public static bool downedInpuratus = false;
         public static bool DashShake = false;
     }
 }
